de5b4da70ed8f51d028004bfc3445b2e96ae0679
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2022-07-19 15:03:38 +0700

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
+import { CronJobPreview } from './pages/CronJobPreview'
 
 export const App = () => {
-  return <></>;
+  return <><CronJobPreview /></>;
 };
